d2b844f1baf6fbef134dbb6e7c1e6284655cf483
Eugene <bubeev@inbox.ru>
2023-04-24 12:32:47 +0100

    add creating a group of bullets, and add it to main cycle

--- aliens.py
+++ aliens.py
@@ -4,17 +4,23 @@ from settings import Settings
 from ship import Ship
 from naruto import Naruto 
 import game_function as gf
+from pygame.sprite import Group
 def run_game():
     pygame.init()
     ai_settings=Settings()
     screen=pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
+    #creating a ship
     ship=Ship(ai_settings,screen)
+    #creating a group to store bullets
+    bullets = Group()
     naruto=Naruto(screen)
     pygame.display.set_caption("Alien invasion")
+    #Start main cicle of the game
     while True:
-        gf.check_ivents(ship)
-        gf.update_screen(ai_settings,screen,ship,naruto)
+        gf.check_ivents(ai_settings,ship,bullets)
+        gf.update_screen(ai_settings,screen,ship,naruto,bullets)
         ship.update()
+        bullets.update()
         
 run_game()
 
