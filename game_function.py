9fbaeaa02e6ea4e5bcee4ba0ca463a366faf184d
Eugene <bubeev@inbox.ru>
2023-04-25 09:49:02 +0100

    Add bullet into the check event and check event keydown, also in update_screen

--- game_function.py
+++ game_function.py
@@ -2,7 +2,7 @@ import pygame
 import sys
 from bullet import Bullet
 #tracking keyboard and mouse movements
-def check_keydown_events(event,ship, screen, ai_settings, bullets):
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     if event.key==pygame.K_RIGHT:
     #moving ship right
         ship.moving_right=True
@@ -17,19 +17,18 @@ def check_keyup_events(event,ship):
         ship.moving_right=False
     elif event.key==pygame.K_LEFT:
         ship.moving_left=False
-def check_events(ship):
+def check_events(ai_settings, screen, ship, bullets):
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             exit()
         elif event.type==pygame.KEYDOWN:
-            check_keydown_events(event,ship, ai_settings, screen, bullets)
+            check_keydown_events(event,ai_settings, screen, ship, bullets)
         elif event.type==pygame.KEYUP:
             check_keyup_events(event,ship)
         
-def update_screen(ai_settings,screen,ship,naruto):
+def update_screen(ai_settings, screen, ship, bullets):
     #The screen is redrawn on each iteration of the loop
     screen.fill(ai_settings.bg_colour)
-    naruto.blitme()
     #All the bullets draw behinde the ship and alien
     for bullet in bullets.sprites():
         bullet.draw_bullet()
