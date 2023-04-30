ed6cd4854faf29812c436bf5d605581c28220cf6
Eugene <bubeev@inbox.ru>
2023-04-30 12:39:53 +0100

    Check if alien reach the edge and drop fleet with changing direction

--- alien.py
+++ alien.py
@@ -18,9 +18,17 @@ class Alien(Sprite):
         self.rect.y=self.rect.height
         #Save exact alien position 
         self.x=float(self.rect.x)
+    def check_edges(self):
+        """Retern True if alien near edge"""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right:
+            return True
+        elif self.rect.left <= 0:
+            return True
     def update(self):
         """Move alien ship to the right"""
-        self.x += self.ai_settings.alien_speed_factor
+        self.x += (self.ai_settings.alien_speed_factor *
+                   self.ai_settings.fleet_direction)
         self.rect.x = self.x
         
     def blitme(self):
--- game_function.py
+++ game_function.py
@@ -71,8 +71,20 @@ def create_fleet(ai_settings, screen,ship, aliens):
     for row_number in range(number_rows):
         for alien_number in range(number_aliens_x):
             create_alien(ai_settings, screen, aliens, alien_number, row_number)
-def update_aliens(aliens):
-    """Update position all aliens in the fleet"""
+def check_fleet_edges(ai_settings, aliens):
+    """React when alien reach edges"""
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings, aliens)
+            break
+def change_fleet_direction(ai_settings, aliens):
+    """Down all fleet and change direction"""
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+        ai_settings.fleet_direction *= -1
+def update_aliens(ai_settings, aliens):
+    """check if alien rech edge and update position all aliens in the fleet"""
+    check_fleet_edges(ai_settings, aliens)
     aliens.update()
 
 def get_number_star_x(ai_settings, star_width):
