256823f5d18d6665298acafd0f303c88947afcda
Eugene <bubeev@inbox.ru>
2023-05-22 16:38:38 +0100

    Add dynamic

--- settings.py
+++ settings.py
@@ -9,18 +9,21 @@ class Settings:
         self.screen_height=750
         self.bg_colour=(5,19,38)
         #ship parametrs
-        self.ship_speed_factor=1.5
         self.ship_limit = 3
         #bullet parameters
-        self.bullet_speed_factor = 1
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 250, 250, 250
         self.bullets_allowed = 3
         #aliens parameters
-        self.alien_speed_factor = 1
         self.fleet_drop_speed = 50
         #fleet direction =1 moving to the right if -1 to the left
         self.fleet_direction = 1
-
+        #temp speed up 
+        self.speedup_scale = 1.5
+        self.initialize_dynamic_settings()
+    def initialize_dynamic_settings(self):
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 1
+        self.alien_speed_factor = 1
     
